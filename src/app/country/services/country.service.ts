cb073f578162dc71c812c9b9332fbb9ffd6147aa
Victor Garcia <victor.garciagutierrez.itcm@gmail.com>
2025-06-13 06:39:06 -0600

    feature: Add debounce in input

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mapper/country.mapper';
 
